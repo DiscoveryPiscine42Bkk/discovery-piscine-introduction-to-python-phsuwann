f44ea2a1488a0c35ee8e079e51a1bf57a1f58bcd
phsuwann <68040826@kmitl.ac.th>
2025-07-22 15:57:18 +0000

    add42

---
+++ cell05/ex09/scan_it.py
@@ -0,0 +1,13 @@
+ #! /usr/bin/env python3
+import sys
+
+args = sys.argv[1:]
+
+if len(args) != 2:
+    print ("none")
+else:
+    word = args[0] 
+    text = args[1]
+    count = text.split ().count(word)
+    print(count)
+    
